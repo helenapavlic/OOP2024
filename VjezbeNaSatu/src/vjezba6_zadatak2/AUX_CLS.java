ff728ff535d11acc61d01cfc8f0f47fccbadc8ff
Helena <helenapavlic402@gmail.com>
2024-05-06 18:38:16 +0200

    vjezba 6

--- VjezbeNaSatu/src/vjezba6_zadatak2/AUX_CLS.java
+++ VjezbeNaSatu/src/vjezba6_zadatak2/AUX_CLS.java
@@ -1,7 +1,7 @@
 package vjezba6_zadatak2;
 
-import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 public class AUX_CLS {
     public static void addNewUser(User user, Map<Integer, String> users) {
@@ -13,18 +13,46 @@ public class AUX_CLS {
         }
     }
 
-//    parametrization
-    public static <K,V> void listAllMapElements(Map<K,V> map) {
+    //    parametrization
+    public static <K, V> void listAllMapElements(Map<K, V> map) {
         System.out.println("-----------------------------------------");
         for (K key : map.keySet()) {
             System.out.println("id: " + key + " username: " + map.get(key));
         }
     }
 
-    public static <K,V> void listAllMapElementsNew(Map<K,V> map){
+    public static <K, V> void listAllMapElementsNew(Map<K, V> map) {
         System.out.println("-----------------------------------------");
-        for (Map.Entry<K, V> entrySet : map.entrySet()){
+        for (Map.Entry<K, V> entrySet : map.entrySet()) {
             System.out.println("Key: " + entrySet.getKey() + " value: " + entrySet.getValue());
         }
     }
+
+    public static void addElementToSet(String element, Set<String> elements) {
+//        set cant have duplicates
+        if (elements.add(element)) {
+            System.out.println("element added!");
+        } else {
+            System.out.println("element is already in set!");
+        }
+    }
+
+
+    public static <E> void printElementsFromSet(Set<E> elements) {
+        System.out.println("------------------- " + elements.getClass().getSimpleName() + " ------------------");
+        for (E element : elements) {
+            System.out.println(element);
+        }
+    }
+
+    public static <E> void removeElementFromSet(Set<E> elements, E elementToRemove) {
+        System.out.println("-----------------------------------------------------");
+        if (elements.remove(elementToRemove)) {
+            System.out.println("removed element: " + elementToRemove);
+        } else {
+            System.out.println("element is not in set");
+        }
+    }
+
+
 }
--- VjezbeNaSatu/src/vjezba6_zadatak2/ClientApp.java
+++ VjezbeNaSatu/src/vjezba6_zadatak2/ClientApp.java
@@ -1,19 +1,52 @@
 package vjezba6_zadatak2;
 
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.TreeSet;
 
 public class ClientApp {
     public static void main(String[] args) {
-        HashMap<Integer,String> users = new HashMap<>();
+        HashMap<Integer, String> users = new HashMap<>();
 
-        User user1 = new User("user1", "pass1",15);
-        User user2 = new User("user2", "pass2",100);
-        User user3 = new User("user3", "pass3",345);
-        AUX_CLS.addNewUser(user1,users);
-        AUX_CLS.addNewUser(user1,users);
-        AUX_CLS.addNewUser(user2,users);
-        AUX_CLS.addNewUser(user3,users);
+        User user1 = new User("user1", "pass1", 15);
+        User user2 = new User("user2", "pass2", 100);
+        User user3 = new User("user3", "pass3", 345);
+        AUX_CLS.addNewUser(user1, users);
+        AUX_CLS.addNewUser(user1, users);
+        AUX_CLS.addNewUser(user2, users);
+        AUX_CLS.addNewUser(user3, users);
         AUX_CLS.listAllMapElements(users);
         AUX_CLS.listAllMapElementsNew(users);
+
+        HashSet<String> hashSet = new HashSet<>();
+        LinkedHashSet<String> linkedHashSet = new LinkedHashSet<>();
+        TreeSet<String> treeSet = new TreeSet<>();
+
+        AUX_CLS.addElementToSet("otvoreni", hashSet);
+        AUX_CLS.addElementToSet("kod", hashSet);
+        AUX_CLS.addElementToSet("linux", hashSet);
+        AUX_CLS.addElementToSet("kripto", hashSet);
+
+        AUX_CLS.addElementToSet("otvoreni", linkedHashSet);
+        AUX_CLS.addElementToSet("kod", linkedHashSet);
+        AUX_CLS.addElementToSet("linux", linkedHashSet);
+        AUX_CLS.addElementToSet("kripto", linkedHashSet);
+
+        AUX_CLS.addElementToSet("otvoreni", treeSet);
+        AUX_CLS.addElementToSet("kod", treeSet);
+        AUX_CLS.addElementToSet("linux", treeSet);
+        AUX_CLS.addElementToSet("kripto", treeSet);
+        AUX_CLS.addElementToSet("kripto", treeSet);
+
+        AUX_CLS.printElementsFromSet(hashSet);
+        AUX_CLS.printElementsFromSet(treeSet);
+        AUX_CLS.printElementsFromSet(linkedHashSet);
+
+        AUX_CLS.removeElementFromSet(treeSet, "kripto");
+        AUX_CLS.removeElementFromSet(treeSet, "kripto");
+        AUX_CLS.removeElementFromSet(treeSet, "jhasfk");
+        AUX_CLS.printElementsFromSet(treeSet);
+
     }
 }
