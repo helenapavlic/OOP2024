dbb44fe8c52d8830c40acbf95b608610d5562e9a
Helena <helenapavlic402@gmail.com>
2024-05-13 18:20:50 +0200

    throws scanner

--- Predavanja/src/pckg_exceptions1/ClientApp.java
+++ Predavanja/src/pckg_exceptions1/ClientApp.java
@@ -1,5 +1,6 @@
 package pckg_exceptions1;
 
+import java.io.FileNotFoundException;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
@@ -7,14 +8,23 @@ public class ClientApp {
 //    exception handling -> try catch ili throws in method signature -> exception propagation
 //    nacin koji odabiremo ovisi gdje je logicnije rjesavati odredeni exception, razlikuje se u praksi
     public static void main(String[] args) {
-        try{
-            Scanner sc = new Scanner(System.in);
-            double number;
-            System.out.print("please enter one number (double): ");
-            number = sc.nextDouble();
-            System.out.println("you have entered: " + number);
-        } catch (InputMismatchException ime){
-            System.out.println("inout is not double");
+//        try{
+//            Scanner sc = new Scanner(System.in);
+//            double number;
+//            System.out.print("please enter one number (double): ");
+//            number = sc.nextDouble();
+//            System.out.println("you have entered: " + number);
+//        } catch (InputMismatchException ime){
+//            System.out.println("inout is not double");
+//        }
+
+//        copy od repository root
+        String filepath = "Predavanja/src/pckg_exceptions1/primjer.txt";
+//        hendling scanner exception from readFromFileMethod
+        try {
+            ReadContentClass.readFromFile(filepath);
+        } catch (FileNotFoundException e) {
+            System.out.println("error");
         }
     }
 }
---
+++ Predavanja/src/pckg_exceptions1/ReadContentClass.java
@@ -0,0 +1,22 @@
+package pckg_exceptions1;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class ReadContentClass {
+
+    public static void readFromFile(String filePath) throws FileNotFoundException {
+        File file = new File(filePath);
+//        try {
+        Scanner scanner = new Scanner(file);
+        while (scanner.hasNext()) {
+            String line = scanner.nextLine();
+            System.out.println("Line: " + line);
+//            }
+//        } catch (FileNotFoundException e) {
+//            System.out.println("error");
+//        }
+        }
+    }
+}
