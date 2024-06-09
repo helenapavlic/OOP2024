992ed41b4c2245b2cfbb648b957b1ad425b4e365
Helena <helenapavlic402@gmail.com>
2024-06-09 11:55:07 +0200

    zadatak 6 ispitni rok 18.06.2021. gui player frame view

---
+++ GUI/src/zad_6_18_06_21/view/PlayerFrame.java
@@ -0,0 +1,47 @@
+package zad_6_18_06_21.view;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class PlayerFrame extends JFrame {
+//    kao main frame
+    BasicInfoPanel basicInfoPanel;
+    GamesPanel gamesPanel;
+    ToolBar toolBar;
+
+    public PlayerFrame(){
+        super("Player");
+        initMainFrame();
+        initComponents();
+        layoutComponents();
+        activateMainFrame();
+    }
+
+    private void activateMainFrame() {
+//        zadnje, postavlja listenere za svaki gumb koristeci anonimnu klasu
+
+    }
+
+    private void layoutComponents() {
+//        layout panele na ekranu
+        setLayout(new BorderLayout());
+
+        add(basicInfoPanel,BorderLayout.CENTER);
+        add(gamesPanel,BorderLayout.LINE_END);
+        add(toolBar,BorderLayout.SOUTH);
+    }
+
+    private void initComponents() {
+        basicInfoPanel = new BasicInfoPanel();
+        gamesPanel = new GamesPanel();
+        toolBar = new ToolBar();
+    }
+
+    private void initMainFrame() {
+        setSize(700, 580);
+        setVisible(true);
+        setLocationRelativeTo(null);
+        setResizable(false);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+}
